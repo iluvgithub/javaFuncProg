82e5ac5dab7848db2960d0d15888ed2dfcc87371
pascal bossavit <pascalbossavit@imac-de-pascal.home>
2016-08-25 01:06:32 +0200

    first running tests

---
+++ src/main/java/com/sandbox/funcprog/list/List.java
@@ -0,0 +1,77 @@
+package com.sandbox.funcprog.list;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public abstract class List<A> {
+
+	public static <T> List<T> cons(T head, List<T> tail) {
+		return new Cons<T>(head, tail);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> List<T> nil() {
+		return (Nil<T>) Nil.instance;
+	}
+
+	public abstract <B> B foldRight(B constant, BiFunction<A, B, B> biFunc);
+
+	public List<A> substract(List<A> list) {
+		return null;
+	}
+
+	public abstract Boolean isEqualTo(List<A> list);
+
+	public <B> List<B> map(Function<A, B> func) {
+		final List<B> seed = nil();
+		return foldRight(seed, (a, list) -> cons(func.apply(a), list));
+	}
+
+	public List<A> cat(List<A> list) {
+		return foldRight(list, (a, as) -> cons(a, as));
+	}
+
+	private final static class Cons<A> extends List<A> {
+
+		private final A head;
+		private final List<A> tail;
+
+		public Cons(A head, List<A> tail) {
+			super();
+			this.head = head;
+			this.tail = tail;
+		}
+
+		@Override
+		public <B> B foldRight(B seed, BiFunction<A, B, B> biFunc) {
+			return biFunc.apply(head, tail.foldRight(seed, biFunc));
+		}
+
+		@Override
+		public Boolean isEqualTo(List<A> list) {
+			return list != Nil.instance && isEqualToCons((Cons<A>) list);
+		}
+
+		private Boolean isEqualToCons(Cons<A> list) {
+			return head.equals(list.head) && tail.isEqualTo(list.tail);
+		}
+
+	}
+
+	private final static class Nil<A> extends List<A> {
+
+		private static final Nil<Object> instance = new Nil<>();
+
+		@Override
+		public <B> B foldRight(B seed, BiFunction<A, B, B> biFunc) {
+			return seed;
+		}
+
+		@Override
+		public Boolean isEqualTo(List<A> list) {
+			return this == list;
+		}
+
+	}
+
+}
---
+++ src/test/java/com/sandbox/funcprog/list/ListTest.java
@@ -0,0 +1,76 @@
+package com.sandbox.funcprog.list;
+
+import static com.sandbox.funcprog.list.List.cons;
+import static com.sandbox.funcprog.list.List.nil;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ListTest {
+
+	@Test
+	public void testSumRight() throws Exception {
+		// arrange
+		List<Integer> input = cons(1, cons(2, cons(3, nil())));
+		// act
+		Integer actual = input.foldRight(0, (a, b) -> a + b);
+		// assert
+		assertEquals(6, actual.intValue());
+	}
+
+	@Test
+	public void testBigSum() throws Exception {
+		// arrange
+		int n = 10; //000;
+		List<Integer> input = nil();
+		for (int i = 0; i < n; ++i) {
+			input = cons(i + 1, input);
+		}
+		// act
+		Integer actual = input.foldRight(0, (a, b) -> a + b);
+		// assert
+		assertEquals(n*(n+1)/2, actual.intValue());
+	}
+
+	@Test
+	public void testEquals() throws Exception {
+		// arrange
+		List<Integer> input1 = cons(1, cons(2, cons(3, nil())));
+		List<Integer> input2 = cons(1, cons(2, cons(3, nil())));
+		List<Integer> input3 = cons(0, cons(1, cons(2, cons(3, nil()))));
+		List<Integer> nil1 = nil();
+		List<Integer> nil2 = nil();
+		// assert
+		assertTrue(input1.isEqualTo(input2));
+		assertFalse(input1.isEqualTo(input3));
+
+		assertTrue(nil1.isEqualTo(nil2));
+		assertFalse(nil1.isEqualTo(input1));
+		assertFalse(input1.isEqualTo(nil1));
+	}
+
+	@Test
+	public void testMap() throws Exception {
+		// arrange
+		List<Integer> input = cons(1, cons(2, cons(3, nil())));
+		// act
+		List<String> actual = input.map(i -> i.toString());
+		// assert
+		List<String> expected = cons("1", cons("2", cons("3", nil())));
+		assertTrue(expected.isEqualTo(actual));
+	}
+
+	@Test
+	public void testCat() throws Exception {
+		// arrange
+		List<Integer> input1 = cons(1, cons(2, cons(3, nil())));
+		List<Integer> input2 = cons(4, cons(5, nil()));
+		// act
+		List<Integer> actual = input1.cat(input2);
+		// assert
+		List<Integer> expected = cons(1, cons(2, cons(3, cons(4, cons(5, nil())))));
+		assertTrue(expected.isEqualTo(actual));
+	}
+}
