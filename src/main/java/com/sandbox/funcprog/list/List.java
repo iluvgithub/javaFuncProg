c24dba38dbf404743bc22ff6209f6c2113eb37f9
pascal bossavit <pascalbossavit@imac-de-pascal.home>
2016-09-09 21:24:45 +0200

    added unfold

--- src/main/java/com/sandbox/funcprog/list/List.java
+++ src/main/java/com/sandbox/funcprog/list/List.java
@@ -1,12 +1,17 @@
 package com.sandbox.funcprog.list;
 
+import static com.sandbox.funcprog.pair.Pair.pair;
+import static com.sandbox.funcprog.recursion.TailRecursion.finalCall;
+import static com.sandbox.funcprog.recursion.TailRecursion.suspendCall;
+
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
+import com.sandbox.funcprog.pair.Pair;
 import com.sandbox.funcprog.recursion.TailRecursion;
 
 public abstract class List<A> {
-
 	public static <T> List<T> cons(T head, List<T> tail) {
 		return new Cons<T>(head, tail);
 	}
@@ -22,10 +27,47 @@ public abstract class List<A> {
 
 	protected abstract <B> TailRecursion<B> foldl(B seed, BiFunction<A, B, B> biFunc);
 
-	public abstract Boolean isEqualTo(List<A> list);
+	public static <A, B> List<A> unfold(Predicate<B> keepGoing, Function<B, A> head, Function<B, B> tail, B input) {
+		return unfoldAcc(keepGoing, head, tail, input, nil()).call().reverse();
+	}
+
+	private static <A, B> TailRecursion<List<A>> unfoldAcc(Predicate<B> keepGoing, Function<B, A> hd, Function<B, B> tl,
+			B in, List<A> acc) {
+		if (keepGoing.test(in)) {
+			return suspendCall(() -> unfoldAcc(keepGoing, hd, tl, tl.apply(in), cons(hd.apply(in), acc)));
+		} else {
+			return finalCall(acc);
+		}
+
+	}
+
+	protected abstract Cons<A> toCons();
+
+	public <X, Y> List<Pair<X, Y>> zip(Pair<List<X>, List<Y>> input) {
+		Predicate<Pair<List<X>, List<Y>>> keepGoing = pair -> pair.left() != nil() && pair.right() != nil();
+		Function<Pair<List<X>, List<Y>>, Pair<X, Y>> hd = pair -> pair(pair.left().toCons().head,
+				pair.right().toCons().head);
+		Function<Pair<List<X>, List<Y>>, Pair<List<X>, List<Y>>> tl =
+
+		pair -> pair(pair.left().toCons().tail, pair.right().toCons().tail);
+		return unfold(keepGoing, hd, tl, input);
+	}
+
+	public Boolean isEqualTo(List<A> list) {
+		if (!this.size().equals(list.size())) {
+			return false;
+		}
+
+		return zip(pair(this, list)).foldLeft(true, (pair, bool) -> bool && pair.left().equals(pair.right()));
+
+	}
+
+	public Integer size() {
+		return foldLeft(0, (a, i) -> i + 1);
+	}
 
 	public List<A> reverse() {
-		return foldLeft(nil(), (h, l) -> cons(h, l));
+		return foldLeft(nil(), (hd, tl) -> cons(hd, tl));
 	}
 
 	public <B> List<B> map(Function<A, B> func) {
@@ -39,6 +81,7 @@ public abstract class List<A> {
 	private final static class Cons<A> extends List<A> {
 
 		private final A head;
+
 		private final List<A> tail;
 
 		public Cons(A head, List<A> tail) {
@@ -48,17 +91,14 @@ public abstract class List<A> {
 		}
 
 		@Override
+
 		public <B> TailRecursion<B> foldl(B seed, BiFunction<A, B, B> biFunc) {
-			return TailRecursion.suspendCall(() -> tail.foldl(biFunc.apply(head, seed), biFunc));
+			return suspendCall(() -> tail.foldl(biFunc.apply(head, seed), biFunc));
 		}
 
 		@Override
-		public Boolean isEqualTo(List<A> list) {
-			return list != Nil.instance && isEqualToCons((Cons<A>) list);
-		}
-
-		private Boolean isEqualToCons(Cons<A> list) {
-			return head.equals(list.head) && tail.isEqualTo(list.tail);
+		protected Cons<A> toCons() {
+			return this;
 		}
 
 	}
@@ -69,13 +109,14 @@ public abstract class List<A> {
 
 		@Override
 		protected <B> TailRecursion<B> foldl(B seed, BiFunction<A, B, B> biFunc) {
-			return TailRecursion.finalCall(seed);
+			return finalCall(seed);
 		}
 
 		@Override
-		public Boolean isEqualTo(List<A> list) {
-			return this == list;
-		}
-	}
 
+		protected Cons<A> toCons() {
+			throw new RuntimeException("Should never call Nil::toCons");
+		}
+
+	}
 }
--- src/test/java/com/sandbox/funcprog/list/ListTest.java
+++ src/test/java/com/sandbox/funcprog/list/ListTest.java
@@ -2,11 +2,17 @@ package com.sandbox.funcprog.list;
 
 import static com.sandbox.funcprog.list.List.cons;
 import static com.sandbox.funcprog.list.List.nil;
+import static com.sandbox.funcprog.pair.Pair.pair;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 import org.junit.Test;
+
+import com.sandbox.funcprog.pair.Pair;
 
 public class ListTest {
 
@@ -21,17 +27,34 @@ public class ListTest {
 	}
 
 	@Test
+
 	public void testBigSum() throws Exception {
 		// arrange
 		int n = 100000;
-		List<Integer> input = nil();
-		for (int i = 0; i < n; ++i) {
-			input = cons(i + 1, input);
-		}
+		List<Integer> input = makeBigList(n);
 		// act
 		Integer actual = input.foldLeft(0, (a, b) -> a + b);
 		// assert
 		assertEquals(n * (n + 1) / 2, actual.intValue());
+	}
+
+	private List<Integer> makeBigList(int n) {
+		List<Integer> input = nil();
+		for (int i = 0; i < n; ++i) {
+			input = cons(i + 1, input);
+		}
+		return input;
+	}
+
+	@Test
+	public void testSize() throws Exception {
+		// arrange
+		List<Integer> input = cons(1, cons(2, cons(3, nil())));
+		// act
+		Integer actual = input.size();
+		// assert
+		assertEquals(3, actual.intValue());
+
 	}
 
 	@Test
@@ -40,6 +63,7 @@ public class ListTest {
 		List<Integer> input1 = cons(1, cons(2, cons(3, nil())));
 		List<Integer> input2 = cons(1, cons(2, cons(3, nil())));
 		List<Integer> input3 = cons(0, cons(1, cons(2, cons(3, nil()))));
+		List<Integer> input4 = cons(1, cons(-2, cons(3, nil())));
 		List<Integer> nil1 = nil();
 		List<Integer> nil2 = nil();
 		// assert
@@ -48,6 +72,22 @@ public class ListTest {
 		assertTrue(nil1.isEqualTo(nil2));
 		assertFalse(nil1.isEqualTo(input1));
 		assertFalse(input1.isEqualTo(nil1));
+		assertFalse(input1.isEqualTo(input4));
+	}
+
+	@Test
+
+	public void testEqualsMass() throws Exception {
+		// arrange
+		int n = 100000;
+		List<Integer> input1 = makeBigList(n);
+		List<Integer> input2 = makeBigList(n);
+		// assert
+		assertTrue(input1.isEqualTo(input2));
+		// re-arrange
+		input1 = cons(0, input1);
+		input2 = cons(-1, input2);
+		assertFalse(input1.isEqualTo(input2));
 	}
 
 	@Test
@@ -62,6 +102,7 @@ public class ListTest {
 	}
 
 	@Test
+
 	public void testCat() throws Exception {
 		// arrange
 		List<Integer> input1 = cons(1, cons(2, cons(3, nil())));
@@ -76,11 +117,40 @@ public class ListTest {
 	@Test
 	public void testReverse() throws Exception {
 		// arrange
-		List<Integer> input = cons(1, cons(2, cons(3, nil())));
+		List<Integer> input = cons(1, cons(2, cons(3, cons(4, cons(5, nil())))));
 		// act
 		List<Integer> actual = input.reverse();
 		// assert
-		List<Integer> expected = cons(3, cons(2, cons(1, nil())));
+		List<Integer> expected = cons(5, cons(4, cons(3, cons(2, cons(1, nil())))));
 		assertTrue(expected.isEqualTo(actual));
 	}
+
+	@Test
+
+	public void testUnfold() throws Exception {
+		// arrange
+		Predicate<Integer> keepGoing = n -> n < 4;
+		Function<Integer, Integer> head = n -> n;
+		Function<Integer, Integer> tail = n -> n + 1;
+		Integer seed = 1;
+		// act
+		List<Integer> actual = List.unfold(keepGoing, head, tail, seed);
+		// assert
+		List<Integer> expected = cons(1, cons(2, cons(3, nil())));
+		assertTrue(expected.isEqualTo(actual));
+	}
+
+	@Test
+
+	public void testZip() throws Exception {
+		// arrange
+		List<Integer> left = cons(1, cons(2, cons(3, nil())));
+		List<String> right = cons("1", cons("2", cons("3", nil())));
+		// act
+		List<Pair<Integer, String>> actual = nil().zip(pair(left, right));
+		// assert
+		List<Pair<Integer, String>> expected = cons(pair(1, "1"), cons(pair(2, "2"), cons(pair(3, "3"), nil())));
+		assertTrue(expected.isEqualTo(actual));
+	}
+
 }
