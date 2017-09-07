ea959b923bef24815d60df3c248471f0ea19e415
pascalbossavit <pascalbossavit@imac-de-pascal.home>
2017-09-07 23:43:59 +0200

    added assocr

--- src/main/java/com/sandbox/funcprog/bifunctor/Sum.java
+++ src/main/java/com/sandbox/funcprog/bifunctor/Sum.java
@@ -18,10 +18,15 @@ public abstract class Sum<LEFT, RIGHT> implements BiFunctor<LEFT, RIGHT> {
 	public static <T> Function<T, Sum<T, T>> asSwitch(Predicate<T> p) {
 		return t -> p.test(t) ? left(t) : right(t);
 	}
-	
+
 	@Override
 	public <Y1, Y2> Sum<Y1, Y2> map(Function<LEFT, Y1> f, Function<RIGHT, Y2> g) {
 		return apply(l -> left(f.apply(l)), r -> right(g.apply(r)));
+	}
+
+	// (a+b)+c -> a+(b+c)
+	public static <A, B, C> Sum<A, Sum<B, C>> assocr(Sum<Sum<A, B>, C> input) {
+		return input.apply(ab -> ab.apply(a -> left(a), b -> right(new Left<>(b))), c -> right(new Right<>(c)));
 	}
 
 	private static final class Left<LEFT, RIGHT> extends Sum<LEFT, RIGHT> {
