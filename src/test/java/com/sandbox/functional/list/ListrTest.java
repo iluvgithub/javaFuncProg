198a28081a462a1defadd7432cd80ec092091345
pascalbossavit <pascalbossavit@imac-de-pascal.home>
2017-09-28 22:19:49 +0200

    listr

---
+++ src/test/java/com/sandbox/functional/list/ListrTest.java
@@ -0,0 +1,49 @@
+package com.sandbox.functional.list;
+
+import static com.sandbox.functional.list.Listr.cons;
+import static com.sandbox.functional.list.Listr.nil;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.function.BiFunction;
+
+import org.junit.Test;
+
+import com.sandbox.funcprog.list.ListNil;
+
+public class ListrTest {
+
+	@Test
+	public void testTrace() throws Exception {
+		// arrange
+		Listr<Integer> list1 = cons(3, nil());
+		Listr<Integer> list = cons(1, cons(2, cons(3, nil())));
+		// act
+		String actual1 = list1.trace();
+		String actual = list.trace();
+		// assert
+		assertThat(actual1).isEqualTo("[3]");
+		assertThat(actual).isEqualTo("[1.2.3]");
+	}
+
+	@Test
+	public void testMap() throws Exception {
+		// arrange
+		Listr<String> list = cons("a", cons("b", cons("c", nil())));
+		// act
+		Listr<String> actuals = list.map(String::toUpperCase);
+		// assert
+		//assertThat(actuals.trace()).isEqualTo("[A.B.C]");
+	}
+
+	@Test
+	public void testFoldr() throws Exception {
+		// arrange
+		Listr<Integer> list = cons(1, cons(2, cons(3, nil())));
+		BiFunction<Integer, String, String> bi = (a, b) -> "(" + a + "+" + b + ")";
+		// act
+		String actual = list.foldr("0", bi);
+		// assert
+		assertThat(actual).isEqualTo("(1+(2+(3+0)))");
+	}
+
+}
