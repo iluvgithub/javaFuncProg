f6e062ba51e31e28ca335e478da1058a47d70c9d
pascalbossavit <pascalbossavit@imac-de-pascal.home>
2017-10-01 23:10:00 +0200

    removed too early abstraction

--- src/main/java/com/sandbox/functional/list/ListlFunctor.java
+++ src/main/java/com/sandbox/functional/list/ListlFunctor.java
@@ -1,17 +1,22 @@
 package com.sandbox.functional.list;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
+import com.sandbox.functional.bifunctor.BiFunctor;
 import com.sandbox.functional.bifunctor.Both;
 import com.sandbox.functional.bifunctor.Either;
+import com.sandbox.functional.visitor.SeedFunctor;
 import com.sandbox.functional.visitor.SeedFunctorVisitor;
 
 //X ->  1 +  XxT
 //f -> Id +  fxIdT
-public class ListlFunctor<T, X> extends AbstractListFunctor<T, X, X, T> {
+public class ListlFunctor<T, X> implements SeedFunctor<T, X>, BiFunctor<T, X> {
+
+	private final Either<Void, Both<X, T>> either;
 
 	protected ListlFunctor(Either<Void, Both<X, T>> either) {
-		super(either);
+		this.either = either;
 	}
 
 	@Override
@@ -21,12 +26,15 @@ public class ListlFunctor<T, X> extends AbstractListFunctor<T, X, X, T> {
 
 	@Override
 	public <Y1, Y2> ListlFunctor<Y1, Y2> map(Function<T, Y1> f, Function<X, Y2> g) {
-		return new ListlFunctor<>(preMap(g, f));
+		return new ListlFunctor<>(either.map(x -> x, both -> both.map(g, f)));
 	}
 
-	@Override
 	public X accept(SeedFunctorVisitor<T, X> visitor) {
 		return visitor.visit(this);
+	}
+
+	protected <Z> Z apply(Z z, BiFunction<X, T, Z> biFunction) {
+		return either.apply(v -> z, both -> both.apply(biFunction));
 	}
 
 }
--- src/main/java/com/sandbox/functional/list/ListrFunctor.java
+++ src/main/java/com/sandbox/functional/list/ListrFunctor.java
@@ -1,17 +1,22 @@
 package com.sandbox.functional.list;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
+import com.sandbox.functional.bifunctor.BiFunctor;
 import com.sandbox.functional.bifunctor.Both;
 import com.sandbox.functional.bifunctor.Either;
+import com.sandbox.functional.visitor.SeedFunctor;
 import com.sandbox.functional.visitor.SeedFunctorVisitor;
 
 //X ->  1 +   TxX
 //f -> Id + IdTxf
-public class ListrFunctor<T, X> extends AbstractListFunctor<T, X, T, X> {
+public class ListrFunctor<T, X> implements SeedFunctor<T, X>, BiFunctor<T, X> {
+
+	private final Either<Void, Both<T, X>> either;
 
 	protected ListrFunctor(Either<Void, Both<T, X>> either) {
-		super(either);
+		this.either = either;
 	}
 
 	@Override
@@ -21,11 +26,15 @@ public class ListrFunctor<T, X> extends AbstractListFunctor<T, X, T, X> {
 
 	@Override
 	public <Y1, Y2> ListrFunctor<Y1, Y2> map(Function<T, Y1> f, Function<X, Y2> g) {
-		return new ListrFunctor<>(preMap(f, g));
+		return new ListrFunctor<>(either.map(x -> x, both -> both.map(f, g)));
 	}
 
-	@Override
 	public X accept(SeedFunctorVisitor<T, X> visitor) {
 		return visitor.visit(this);
 	}
+
+	protected <Z> Z apply(Z z, BiFunction<T, X, Z> biFunction) {
+		return either.apply(v -> z, both -> both.apply(biFunction));
+	}
+
 }
