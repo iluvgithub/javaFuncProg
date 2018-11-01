aea4e96168ddceb639730f957622f1a0c9315576
iluvgithub <pascal.bossavit@gmail.com>
2018-11-01 19:11:23 +0100

    servlets

---
+++ src/main/java/com/web/servlet/HelloWorldServlet.java
@@ -0,0 +1,32 @@
+package com.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * https://javatutorial.net/java-servlet-example
+ *
+ */
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = -4751096228274971485L;
+
+	@Override
+	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped");
+	}
+}
