e659ef85dd6b690c539daf527d6c191d254cb6f6
iluvgithub <pascal.bossavit@gmail.com>
2018-11-14 19:33:27 +0100

    skeletton

---
+++ src/test/java/com/func/parse/ExpressionTest.java
@@ -0,0 +1,18 @@
+package com.func.parse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExpressionTest {
+
+	@Test
+	public void testName() {
+		// given
+
+		// when
+		//Assert.fail("Not yet implemented");
+		// then
+		
+	}
+
+}
