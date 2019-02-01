08cf5f63a2c8f188f29eb0cb72b490bb283c122f
iluvgithub <pascal.bossavit@gmail.com>
2019-02-02 00:36:33 +0100

    unfold

--- src/main/java/com/func/stream/StreamUtil.java
+++ src/main/java/com/func/stream/StreamUtil.java
@@ -1,11 +1,13 @@
 package com.func.stream;
 
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import com.func.Prod;
+import com.func.tailrec.Bouncer;
 
 public class StreamUtil {
 
@@ -18,6 +20,12 @@ public class StreamUtil {
 
 	private static <X, Z> BinaryOperator<Prod<X, Z>> reducer(BiFunction<Z, X, Z> bi) {
 		return (xz1, xz2) -> Prod.<X, Z>prod(xz2.left(), bi.apply(xz1.right(), xz2.left()));
+	}
+
+	public static <S, A> Function<S, Stream<A>> unfold(Function<S, Optional<Prod<A, S>>> g) {
+		return s -> Bouncer.hylo(g, Stream.<A>builder(), //
+				(bld, a) -> bld.add(a)).//
+				apply(s).build();
 	}
 
 }
--- src/test/java/com/func/stream/StreamUtilTest.java
+++ src/test/java/com/func/stream/StreamUtilTest.java
@@ -2,9 +2,13 @@ package com.func.stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.junit.Test;
+
+import com.func.Prod;
 
 public class StreamUtilTest {
 
@@ -16,6 +20,23 @@ public class StreamUtilTest {
 		Double actual = StreamUtil.<Integer, Double>foldl((a, b) -> a + b, 0.0).apply(str);
 		// then
 		assertThat(actual).isEqualTo(10.0);
+	}
+
+	@Test
+	public void testUnfold() {
+		// given
+		String s0 = "";
+		String s1 = "abc";
+		Function<String, Optional<Prod<Character, String>>> g = x -> Optional.of(x).//
+				filter(s -> s.length() > 0).//
+				map(s -> Prod.prod(s.charAt(0), s.substring(1)));
+		Function<String, Stream<Character>> h = StreamUtil.unfold(g);
+		// when
+		String actual0 = h.apply(s0).map(Object::toString).reduce("", (s, c) -> s + c);
+		String actual1 = h.apply(s1).map(Object::toString).reduce("", (s, c) -> s + c);
+		// then
+		assertThat(actual0).isEqualTo(s0);
+		assertThat(actual1).isEqualTo(s1);
 	}
 
 }
