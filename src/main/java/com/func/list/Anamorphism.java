3624856f92e40460829a36b427aefcbc6bbc7234
iluvgithub <pascal.bossavit@gmail.com>
2019-02-06 23:01:58 +0100

    refactoring

--- src/main/java/com/func/list/Anamorphism.java
+++ src/main/java/com/func/list/Anamorphism.java
@@ -14,118 +14,94 @@ import com.func.monad.MonadicSupplier;
 
 public class Anamorphism {
 
-    public static <S, X> Function<S, List<X>> unfold(Predicate<S> isOver, //
-                 Function<S, S> nextState, //
-                 Function<S, X> newElement) {
-          return unfold(isOver, Prod.bracket(newElement, nextState));
-    }
+	public static <S, X> Function<S, List<X>> unfold(Predicate<S> isOver, //
+			Function<S, S> nextState, //
+			Function<S, X> newElement) {
+		return unfold(isOver, Prod.bracket(newElement, nextState));
+	}
 
+	public static <S, X> Function<S, List<X>> unfold(Predicate<S> isOver, Function<S, Prod<X, S>> g) {
+		return unfold(convert(isOver, g));
+	}
 
-    public static <S, X> Function<S, List<X>> unfold(Predicate<S> isOver, Function<S, Prod<X, S>> g) {
-          return unfold(convert(isOver, g));
-    }
+	protected static <S, X> Function<S, Optional<Prod<X, S>>> convert(Predicate<S> isOver, Function<S, Prod<X, S>> g) {
+		return s -> Optional.of(s).filter(isOver.negate()).map(g);
+	}
 
+	public static <S, X> Function<S, List<X>> unfold(Function<S, Optional<Prod<X, S>>> g) {
+		return g.andThen(MonadicSupplier::unit).//
+				andThen(mapWithListFunctor(() -> unfold(g))).//
+				andThen(Anamorphism::in);
+	}
 
-    protected static <S, X> Function<S, Optional<Prod<X, S>>> convert(Predicate<S> isOver, Function<S, Prod<X, S>> g) {
-          return s -> Optional.of(s).filter(isOver.negate()).map(g);
-    }
+	private static <A, U, V> Function<//
+			MonadicSupplier<Optional<Prod<A, U>>>, //
+			MonadicSupplier<Optional<Prod<A, V>>>//
+	> mapWithListFunctor(MonadicSupplier<Function<U, V>> f) {
+		return supplier -> supplier.flatMap(//
+				opt -> MonadicSupplier.unit(opt.map(Prod.cross(identity(), f.get())))
+		//
+		);
+	}
 
+	public static <X> List<X> list(X[] xs) {
+		return Anamorphism.<Prod<X[], Integer>, X>unfold(//
+				Prod -> Prod.left().length == Prod.right(), //
+				Prod.rightMapper(n -> n + 1), //
+				Prod.folder((array, index) -> array[index])).//
+				apply(prod(xs, 0));
 
-    public static <S, X> Function<S, List<X>> unfold(Function<S, Optional<Prod<X, S>>> g) {
-          return g.//
-                        andThen(MonadicSupplier::unit).//
-                        andThen(mapWithListFunctor(() -> unfold(g))).//
-                        andThen(Anamorphism::in);
-    }
+	}
 
+	private static <A> List<A> in(MonadicSupplier<Optional<Prod<A, List<A>>>> supplier) {
+		return () -> supplier.get();
+	}
 
+	public static <A> List<A> concat(List<A> l0, List<A> r0) {
+		return unfold(Anamorphism.<A>concatSeed()).apply(prod(l0, r0));
+	}
 
-    private static <A, U, V> Function<//
-    MonadicSupplier<Optional<Prod<A, U>>>, //
-    MonadicSupplier<Optional<Prod<A, V>>>//
-    > mapWithListFunctor(MonadicSupplier<Function<U, V>> f) {
-          return supplier -> supplier. //
-                        flatMap(//
-                                     optAu -> MonadicSupplier.unit(optAu.map(Prod.cross(identity(), f.get())))
-          //
-          );
+	private static <A> Function<Prod<List<A>, List<A>>, Optional<Prod<A, Prod<List<A>, List<A>>>>> concatSeed() {
+		return lr -> {
+			if (Prod.<List<A>, List<A>, Boolean>folder((l, r) -> l.isEmpty() && r.isEmpty()).apply(lr)) {
+				return Optional.empty();
+			} else {
+				return Prod.<List<A>, List<A>, Optional<Prod<A, Prod<List<A>, List<A>>>>>folder((l, r) -> {
+					if (l.isEmpty()) {
+						return r.out().map(Prod.folder((hd, tl) -> prod(hd, prod(l, tl))));
+					} else {
+						return l.out().map(Prod.folder((hd, tl) -> prod(hd, prod(tl, r))));
+					}
+				}).apply(lr);
+			}
+		};
+	}
 
-    }
+	public static <U, V> Function<List<U>, List<V>> mapper(Function<U, V> f) {
+		return unfold(mapperGenerator(f));
+	}
 
+	private static <U, V> Function<List<U>, Optional<Prod<V, List<U>>>> mapperGenerator(Function<U, V> f) {
+		return l -> l.out().map(Prod.cross(f, identity()));
+	}
 
+	public static <S> Function<S, List<S>> iterate(UnaryOperator<S> u) {
+		return unfold(unused -> false, Prod.bracket(identity(), u));
+	}
 
-    public static <X> List<X> list(X[] xs) {
-          return Anamorphism
-                        .<Prod<X[], Integer>, X> unfold(//
-                                     Prod -> Prod.left().length == Prod.right(), //
-                                     Prod.rightMapper(n -> n + 1), //
-                                     Prod.folder((array, index) -> array[index]))
-                        .//
-                        apply(prod(xs, 0));
+	public static <S> Function<S, List<S>> iterate(Function<S, Optional<S>> f) {
+		return partialIterate(f).andThen(mapper(opt -> opt.orElse(null)));
+	}
 
-    }
+	private static <S> Function<S, List<Optional<S>>> partialIterate(Function<S, Optional<S>> partialFunction) {
+		return iterate(Curry.optionalFlatMapperUnitary(partialFunction)).compose(Optional::of);
+	}
 
+	public static Function<String, List<Character>> stringToList() {
+		return unfold(String::isEmpty, Prod.bracket(s -> s.charAt(0), s -> s.substring(1)));
+	}
 
-    private static <A> List<A> in(MonadicSupplier<Optional<Prod<A, List<A>>>> supplier) {
-          return () -> supplier.get();
-    }
-
-
-
-    public static <A> List<A> concat(List<A> l0, List<A> r0) {
-          Function<Prod<List<A>, List<A>>, Optional<Prod<A, Prod<List<A>, List<A>>>>> g = lr -> {
-                 if (Prod.<List<A>, List<A>, Boolean> folder((l, r) -> l.isEmpty() && r.isEmpty()).apply(lr)) {
-                        return Optional.empty();
-
-                 } else {
-                        return Prod.<List<A>, List<A>, Optional<Prod<A, Prod<List<A>, List<A>>>>> folder((l, r) -> {
-                               if (l.isEmpty()) {
-                                     return Optional.of(prod(r.head().get(), prod(l, r.tail().get())));
-                               } else {
-                                     return Optional.of(prod(l.head().get(), prod(l.tail().get(), r)));
-                               }
-                        }).apply(lr);
-                 }
-          }         ;
-
-          return unfold(g).apply(prod(l0, r0));
-    }
-
-
-
-    public static <U, V> Function<List<U>, List<V>> mapper(Function<U, V> f) {
-
-          return unfold(mapperGenerator(f));
-
-    }
-
-
-
-    private static <U, V> Function<List<U>, Optional<Prod<V, List<U>>>> mapperGenerator(Function<U, V> f) {
-          return l -> l.out().map(Prod.cross(f, identity()));
-    }
-
-
-
-    public static <S> Function<S, List<S>> iterate(UnaryOperator<S> u) {
-          return unfold(unused -> false, Prod.bracket(identity(), u));
-    }
-
-
-
-    public static <S> Function<S, List<S>> iterate(Function<S, Optional<S>> f) {
-          return partialIterate(f).andThen(mapper(opt -> opt.orElse(null)));
-    }
-
-
-
-    private static <S> Function<S, List<Optional<S>>> partialIterate(Function<S, Optional<S>> partialFunction) {
-          return iterate(Curry.optionalFlatMapperUnitary(partialFunction)).compose(Optional::of);
-    }
-
-
-
-    public static Function<String, List<Character>> stringToList() {
-          return unfold(String::isEmpty, Prod.bracket(s -> s.charAt(0), s -> s.substring(1)));
-    }
+	public static List<Character> stringToList(String s) {
+		return stringToList().apply(s);
+	}
 }
