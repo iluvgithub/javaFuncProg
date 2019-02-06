cb277b30fa0acd913a3eca5f8a6a2034fc5e000b
iluvgithub <pascal.bossavit@gmail.com>
2019-02-06 22:07:32 +0100

    removed classes

--- src/main/java/com/func/Curry.java
+++ src/main/java/com/func/Curry.java
@@ -1,8 +1,10 @@
 package com.func;
 
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Curry {
 
@@ -22,13 +24,12 @@ public class Curry {
 		return asBinaryOperator(uncurry(f));
 	}
 
-	public static <A> BinaryOperator<A> asBinaryOperator(BiFunction<A, A, A> f) {
-		return (a0, a1) -> f.apply(a0, a1);
+	public static <A> UnaryOperator<A> toUnaryOperator(Function<A, A> f) {
+		return x -> f.apply(x);
 	}
 
-	public static <A, B, U, V, Z> BiFunction<A, B, Z> compose(BiFunction<U, V, Z> bif, Function<A, U> f,
-			Function<B, V> g) {
-		return (a, b) -> bif.apply(f.apply(a), g.apply(b));
+	public static <A> BinaryOperator<A> asBinaryOperator(BiFunction<A, A, A> f) {
+		return (a0, a1) -> f.apply(a0, a1);
 	}
 
 	public static <A, B, Z> Function<A, Z> partialLeft(BiFunction<A, B, Z> bif, B b) {
@@ -37,5 +38,18 @@ public class Curry {
 
 	public static <A, B, Z> Function<B, Z> partialRight(A a, BiFunction<A, B, Z> bif) {
 		return b -> bif.apply(a, b);
+	}
+	/// Optional Specific
+
+	public static <X, Y> Function<Optional<X>, Optional<Y>> optionalMapper(Function<X, Y> mapper) {
+		return opt -> opt.map(mapper);
+	}
+
+	public static <X, Y> Function<Optional<X>, Optional<Y>> optionalFlatMapper(Function<X, Optional<Y>> mapper) {
+		return opt -> opt.flatMap(mapper);
+	}
+
+	public static <X> UnaryOperator<Optional<X>> optionalFlatMapperUnitary(Function<X, Optional<X>> mapper) {
+		return toUnaryOperator(optionalFlatMapper(mapper));
 	}
 }
--- src/test/java/com/func/CurryTest.java
+++ src/test/java/com/func/CurryTest.java
@@ -4,8 +4,10 @@ import static com.func.Curry.curry;
 import static com.func.Curry.uncurry;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import org.junit.Test;
 
@@ -33,4 +35,32 @@ public class CurryTest {
 		assertThat(act).isEqualTo("1.2");
 	}
 
+	@Test
+
+	public void testOptionalMapperUnitary() {
+		// arrange
+		Function<Integer, Integer> inc = x -> x + 1;
+		Function<Integer, String> mapper = inc.andThen(Object::toString);
+		Function<Optional<Integer>, Optional<String>> f = Curry.optionalMapper(mapper);
+		// act
+		Optional<String> actual = f.apply(Optional.of(3));
+		Optional<String> actual0 = f.apply(Optional.empty());
+		// assert
+		assertThat(actual.get()).isEqualTo("4");
+		assertThat(actual0.isPresent()).isFalse();
+	}
+
+	@Test
+	public void testOptionalFlatMapperUnitary() {
+		// arrange
+		Function<Integer, Integer> inc = x -> x + 1;
+		Function<Integer, Optional<Integer>> mapper = x -> Optional.of(x).map(inc);
+		UnaryOperator<Optional<Integer>> unary = Curry.optionalFlatMapperUnitary(mapper);
+		// act
+		Optional<Integer> actual = unary.apply(Optional.of(3));
+		Optional<Integer> actual0 = unary.apply(Optional.empty());
+		// assert
+		assertThat(actual.get()).isEqualTo(3 + 1);
+		assertThat(actual0.isPresent()).isFalse();
+	}
 }
